153d979433b5e5cb6f368e01da39e7bc98e93920
Saaka-y <188415334+Saaka-y@users.noreply.github.com>
2026-01-24 17:10:08 +0900

    Initial commit

---
+++ python_intro.py
@@ -0,0 +1,22 @@
+name = "Tommy"
+# print(name)
+age = 10 + 8
+# print(age)
+# print(len(name))
+
+fruits = ["apple", "banana", "cherry"]
+# print(fruits[1])
+fruits.append("orange")
+# print(fruits)
+
+user = {"name": "Tommy", "age": 18}
+# print(user)
+user["name"] = "Ai"
+# print(user["name"])
+# print(user.pop("age"))
+# print(user)
+
+user["hobby"] = "reading"
+# print(user)
+
+
